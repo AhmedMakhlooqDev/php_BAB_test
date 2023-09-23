254a87d91e7b6804ca00efd34e44dc6fb5b99167
AhmedMakhlooqDev <ahmedmakhlooqdev@gmail.com>
2023-09-23 18:55:06 +0300

    added bootstrap header template

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <?php   
 
+include 'header.php';
 
 
 ?>
@@ -14,6 +15,5 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <body>
-<h1>Hello, world!</h1>
 </body>
 </html>
