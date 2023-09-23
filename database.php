ae810626cde54455f084990f56ace96fb40aa4ef
AhmedMakhlooqDev <ahmedmakhlooqdev@gmail.com>
2023-09-23 22:23:01 +0300

    changed database file logic / made database connection is established

--- database.php
+++ database.php
@@ -1,59 +1,25 @@
 <?php
-class Database
-{
-    private  $_connection;
-    private static $_instance; //The single instance
-    private $_host = "localhost";
-    private $_username = "root";
-    private $_password = "";
-    private $_database = "bab_employee_attendance_system";
-
-    /**
-     * return one instance of the Database object
-     *@return Database
-     */
-    public static function getInstance()
-    {
-        if (!self::$_instance) { // If no instance then make one
-            self::$_instance = new self();
-        }
-        return self::$_instance;
-    }
 
 
-    // Constructor
-    private function __construct()
-    {
-        $this->_connection = new mysqli(
-            $this->_host,
-            $this->_username,
-            $this->_password,
-            $this->_database
-        );
+//database credentials
+$host = "localhost";
+$dbname = "bab_employee_attendance_system";
+$username = "root";
+$password = "";
 
-        // Error handling
-        if (mysqli_connect_error()) {
-            trigger_error(
-                "Failed to connect to MySQL: " . mysqli_connect_error(),
-                E_USER_ERROR
-            );
-        }
-        else{
-            echo("This is working");
-        }
-    }
+//MYSQLI initiation
+$mysqli = new mysqli(hostname: $host, 
+                    username: $username, 
+                    password: $password,
+                    database: $dbname);
 
-    //Magic method clone is empty to prevent duplication of connection
-    private function __clone()
-    {
-    }
+// check for database establishment errors
 
-    /**
-     * return mysqli connection
-     *@return mysqli
-     */
-    public function getConnection()
-    {
-        return $this->_connection;
-    }
+if($mysqli->connect_errno){
+    die("Connection Error: " . $mysqli->connect_error);
 }
+elseif($mysqli){
+    echo "connection successful";
+}
+
+return $mysqli;
