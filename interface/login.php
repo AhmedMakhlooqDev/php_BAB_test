594894419195472646b702dd0f79eeb1bb623b92
AhmedMakhlooqDev <ahmedmakhlooqdev@gmail.com>
2023-09-23 21:45:36 +0300

    added login styling

--- interface/login.php
+++ interface/login.php
@@ -1,6 +1,6 @@
 <?php
 
-include 'header.php';
+include '../header.php';
 
 ?>
 
@@ -11,12 +11,14 @@ include 'header.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="../css/login.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 
 <body>
-    
+    <div class="container">
+
     <form>
         <div class="form-group">
             <label for="exampleInputEmail1">Email address</label>
@@ -33,6 +35,9 @@ include 'header.php';
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
+
+    </div>
+    
 </body>
 
 </html>
--- interface/register.php
+++ interface/register.php
@@ -11,21 +11,23 @@ include '../header.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="../css/register.css">
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 
 <body>
+    <div class="container">
+
     <form>
         <div class="form-group">
             <label for="exampleInputEmail1">Username</label>
             <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
         <div class="form-group">
             <label for="exampleInputEmail1">full name</label>
             <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
         <div class="form-group">
             <label for="exampleInputEmail1">Email address</label>
@@ -42,6 +44,8 @@ include '../header.php';
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
+    </div>
+    
 </body>
 
 </html>
